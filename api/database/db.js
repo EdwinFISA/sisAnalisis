31e5076cf5e0531bd53f47a668fc6d773198293b
EdwinFISA <efigueroas3@miumg.edu.gt>
2024-06-14 04:15:59 -0600

    intentando funcion DB

--- api/database/db.js
+++ api/database/db.js
@@ -1,12 +1,9 @@
-require('dotenv').config(); // Cargar variables de entorno desde .env
+// db.js
 const sqlite3 = require("sqlite3").verbose();
-const path = require("path");
 
-const dbPath = process.env.DB_PATH || path.join(__dirname, "books.db"); // Usar la variable de entorno o una ruta por defecto
+const db = new sqlite3.Database("books.db");
 
-const db = new sqlite3.Database(dbPath);
-
-module.exports = {
+exports.modules = {
   sqlite3,
   db,
 };
