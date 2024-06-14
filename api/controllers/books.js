fc2f5ba2f7f948ae09bb4b23364451cc0043e8a2
EdwinFISA <efigueroas3@miumg.edu.gt>
2024-06-14 05:14:28 -0600

    prueba DB

--- api/controllers/books.js
+++ api/controllers/books.js
@@ -1,44 +1,35 @@
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
-// Determinar la ruta de la base de datos, puede ser configurada a través de una variable de entorno
+const dbPath = process.env.DB_PATH || path.resolve(__dirname, "books.db");
+const db = new sqlite3.Database(dbPath);
 
-// Create books table if not exists
-db.run(
-  "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT, ape TEXT, nits TEXT, tel TEXT, dire TEXT, user TEXT, pass TEXT, role TEXT, state TEXT)"
-);
+db.run(`CREATE TABLE IF NOT EXISTS books (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  nom TEXT,
+  ape TEXT,
+  nits TEXT,
+  tel TEXT,
+  dire TEXT,
+  user TEXT,
+  pass TEXT,
+  role TEXT,
+  state TEXT
+)`);
 
-// CRUD operations
-exports.createBook = (nom, ape, nits, tel, dire, user, pass, role, state, callback) => {
-  db.run(
-    "INSERT INTO books (nom, ape, nits, tel, dire, user, pass, role, state) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-    [nom, ape, nits, tel, dire, user, pass, role, state],
-    function (err) {
-      callback(err, this.lastID);
-    }
-  );
+const promisify = (db, method) => {
+  return (...params) => {
+    return new Promise((resolve, reject) => {
+      db[method](...params, function(err, result) {
+        if (err) reject(err);
+        else resolve(result);
+      });
+    });
+  };
 };
 
-exports.getAllBooks = (callback) => {
-  db.all("SELECT * FROM books", callback);
-};
-
-exports.getBookById = (id, callback) => {
-  db.get("SELECT * FROM books WHERE id = ?", [id], callback);
-};
-
-exports.updateBook = (id, nom, ape, nits, tel, dire, user, pass, role, state, callback) => {
-  db.run(
-    "UPDATE books SET nom = ?, ape = ?, nits = ?, tel = ?, dire = ?, user = ?, pass = ?, role = ?, state = ? WHERE id = ?",
-    [nom, ape, nits, tel, dire, user, pass, role, state, id],
-    function (err) {
-      callback(err, this.changes);
-    }
-  );
-};
-
-exports.deleteBook = (id, callback) => {
-  db.run("DELETE FROM books WHERE id = ?", [id], function (err) {
-    callback(err, this.changes);
-  });
-};
+exports.createBook = promisify(db, "run").bind(db, "INSERT INTO books (nom, ape, nits, tel, dire, user, pass, role, state) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+exports.getAllBooks = promisify(db, "all").bind(db, "SELECT * FROM books");
+exports.getBookById = promisify(db, "get").bind(db, "SELECT * FROM books WHERE id = ?");
+exports.updateBook = promisify(db, "run").bind(db, "UPDATE books SET nom = ?, ape = ?, nits = ?, tel = ?, dire = ?, user = ?, pass = ?, role = ?, state = ? WHERE id = ?");
+exports.deleteBook = promisify(db, "run").bind(db, "DELETE FROM books WHERE id = ?");
